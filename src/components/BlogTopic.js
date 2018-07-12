2cd7412cbcac100e9501e58caad79242ad7e6a5f
Matthew Kane <matthewjkane@live.com>
2018-07-12 16:19:05 -0700

    Working on Blogs by Topic. Structure is in place, now just filling in content

--- src/components/BlogTopic.js
+++ src/components/BlogTopic.js
@@ -6,7 +6,33 @@ class BlogTopic extends React.Component {
         return (
             <div className={styles.wrapper}>
                 <div className={styles.blogTopicWrapper}>
-                    
+                    <h1 className={styles.title}>Blogs By Topic</h1>
+                    <div className={styles.cardWrapper}>
+                        <div className={styles.transformerCard}>
+                            <div className={styles.imgDiv}>
+                            </div>
+                            <div className={styles.description}>
+                                <h3>Transformers</h3>
+                                <p>Chassis-Mounted, Toroidal...</p>
+                            </div>
+                        </div>
+                        <div className={styles.displayCard}>
+                            <div className={styles.imgDiv}>
+                            </div>
+                            <div className={styles.description}>
+                                <h3>Displays</h3>
+                                <p>Capacitive vs. Resistive</p>
+                            </div>
+                        </div>
+                        <div className={styles.relayCard}>
+                            <div className={styles.imgDiv}>
+                            </div>
+                            <div className={styles.description}>
+                                <h3>Power Relays</h3>
+                                <p>Solar, Automotive, Power Relays and More...</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
