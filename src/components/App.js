c47c2ac7ad8d24cc41033a20fa254897614eb6cf
Matthew Kane <matthewjkane@live.com>
2018-07-12 15:36:33 -0700

    Worked further on BlogTopic component. Fixed alignment issue on AboutZettler component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import BlogTopic from './BlogTopic';
 import AboutZettler from './AboutZettler';
 import Footer from './Footer';
 import styles from '../css/app.css';
@@ -9,6 +10,7 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <Header />
+                <BlogTopic />
                 <AboutZettler />
                 <Footer />
             </div>
---
+++ src/components/BlogTopic.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../css/blogtopic.css';
+
+class BlogTopic extends React.Component {
+    render() {
+        return (
+            <div className={styles.wrapper}>
+                <div className={styles.blogTopicWrapper}>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BlogTopic;
