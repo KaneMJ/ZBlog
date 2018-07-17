4100b29344b6a1bb23de026b59f3357b91dfe73d
Matthew Kane <matthewjkane@live.com>
2018-07-17 15:35:49 -0700

    Starting to work on mobile responsiveness. Done with header and feature blog, working on blogtopics

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,63 @@ import BlogTopic from './BlogTopic';
 import AboutZettler from './AboutZettler';
 import Footer from './Footer';
 import styles from '../css/app.css';
+import he from 'he';
 
 class App extends React.Component {
+    state = {
+        content: []
+    };
+
+    componentDidMount() {
+        let dataURL = "http://www.zettlerblog.com/wp-json/wp/v2/posts?orderby=date&per_page=1";
+        fetch(dataURL)
+            .then( res => res.json())
+            .then(res => {
+                let addData = [];
+                addData.push(this.grabImage(res[0].content.rendered)),
+                addData.push(this.shortenTitle(res[0].title.rendered)),
+                addData.push(this.stripHtml(res[0].content.rendered)),
+                this.setState({
+                    content : addData
+                })
+            })
+    };
+
+    grabImage(data) {
+        // Grab image link from the post
+        let start = data.indexOf('src="');
+        let end = data.indexOf('"', start+5);
+        let image = data.substring(start+5, end);
+        return image;
+    }
+
+    stripHtml(data) {          
+        // Strip tag elements
+        data = data.replace(/<[^>]+>/g, '');
+        // Decode html entities
+        data = he.decode(data);
+        // Shorten length if too long
+        if(data.length > 220)
+            data = data.substring(0, 220);
+            data = data.concat("[...]");
+        return data;
+    }
+
+    shortenTitle(title) {
+        // Shorten title if too long
+        if(title.length > 35){
+            title = title.substring(0, 35);
+            title = title.concat("...");
+        }
+        return title;
+    }
+
+
     render() {
         return (
             <div className={styles.container}>
                 <Header />
-                <FeaturedBlog />
+                <FeaturedBlog blog={this.state.content}/>
                 <BlogTopic />
                 <AboutZettler />
                 <Footer />
